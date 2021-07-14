80c231f7ee00fb37d92afc0210e4200b78036bd6
RishixD <82691180+RishixD@users.noreply.github.com>
2021-07-14 10:37:11 +0530

    Update maaChodDiVro.py

--- TelethonBot/plugins/maaChodDiVro.py
+++ TelethonBot/plugins/maaChodDiVro.py
@@ -12,7 +12,7 @@ async def _(event):
   await BotzHub.send_message(event.chat_id, f"Done added {text}")
   await event.reply("PERU HERE",
                     buttons=[
-                        [Button.url("𝙼𝚢 𝚌𝚛𝚎𝚊𝚝𝚘𝚛", "t.me/rishi_introvert")]
+                        [Button.url("𝙼𝚢 𝚌𝚛𝚎𝚊𝚝𝚘𝚛", "t.me/RiSHi_iNTROVERT")]
                     ])
 
     
@@ -31,7 +31,7 @@ async def start_all(event):
 ###################################################
 @BotzHub.on(events.callbackquery.CallbackQuery(data="creator"))
 async def creator(event):
-    await event.edit(event.chat_id, "𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙼𝙰𝚂𝚃𝙴𝚁 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴's USERNAME @RiSHi_iNTROVERT  ")
+    await event.edit(event.chat_id, "𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙼𝙰𝚂𝚃𝙴𝚁's 𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 @RiSHi_iNTROVERT")
 
 ########################################################################################################################################
 
